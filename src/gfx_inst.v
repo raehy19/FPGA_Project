dd416dca59c5b5dcd388bba38009d2cc14118788
Raehyeon Jeong <raehy19@gmail.com>
2023-06-21 10:43:17 +0900

    Feat : add sprite Coin center

--- src/gfx_inst.v
+++ src/gfx_inst.v
@@ -14,10 +14,12 @@ module gfx (
 
 );
     wire [7:0] bg_red, bg_green, bg_blue;
+    wire [7:0] student_id_red, student_id_green, student_id_blue;
     wire [7:0] glacier1_red, glacier1_green, glacier1_blue;
     wire [7:0] glacier2_red, glacier2_green, glacier2_blue;
+    wire [7:0] coin_center_red, coin_center_green, coin_center_blue;
     wire [7:0] penguin_red, penguin_green, penguin_blue;
-    wire glacier1_hit, glacier2_hit, penguin_hit;
+    wire student_id_hit, glacier1_hit, glacier2_hit, coin_center_hit, penguin_hit;
 
     background_img bgimg (
         .i_x    (i_x),
@@ -25,6 +27,15 @@ module gfx (
         .o_red  (bg_red),
         .o_green(bg_green),
         .o_blue (bg_blue)
+    );
+
+    sprite_student_id student_id (
+        .i_x         (i_x),
+        .i_y         (i_y),
+        .o_red       (student_id_red),
+        .o_green     (student_id_green),
+        .o_blue      (student_id_blue),
+        .o_sprite_hit(student_id_hit)
     );
 
     sprite_glacier1 glacier1 (
@@ -60,8 +71,22 @@ module gfx (
         .o_sprite_hit(penguin_hit)
     );
 
+    sprite_coin_center coin_center (
+        .i_x         (i_x),
+        .i_y         (i_y),
+        .i_v_sync    (i_v_sync),
+        .o_red       (coin_center_red),
+        .o_green     (coin_center_green),
+        .o_blue      (coin_center_blue),
+        .o_sprite_hit(coin_center_hit)
+    );
+
     always @(*) begin
-        if (glacier1_hit == 1) begin
+        if (student_id_hit == 1) begin
+            o_red   = student_id_red;
+            o_green = student_id_green;
+            o_blue  = student_id_blue;
+        end else if (glacier1_hit == 1) begin
             o_red   = glacier1_red;
             o_green = glacier1_green;
             o_blue  = glacier1_blue;
@@ -73,6 +98,10 @@ module gfx (
             o_red   = penguin_red;
             o_green = penguin_green;
             o_blue  = penguin_blue;
+        end else if (coin_center_hit == 1) begin
+            o_red   = coin_center_red;
+            o_green = coin_center_green;
+            o_blue  = coin_center_blue;
         end else begin
             o_red   = bg_red;
             o_green = bg_green;
